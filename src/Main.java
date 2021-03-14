327ecfa603c7666c8195cb4c2dda0022a950dcf4
Diana Krapivina <mr.ikoma@mail.ru>
2021-03-14 23:27:18 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int balance = 100;
+        int refill = 1100;
+        int totalSum;
+        if (refill > 1000) {
+            int bonus = refill / 100;
+            totalSum = bonus + refill + balance;
+        } else {
+            totalSum = balance + refill;
+        }
+        System.out.println("Итоговая сумма равна " + totalSum);
+    }
+}
